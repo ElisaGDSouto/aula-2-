e92ae191fdadab77172982e4c463c35326c02d9c
ElisaGDSouto <99265708+ElisaGDSouto@users.noreply.github.com>
2022-02-13 09:48:51 -0300

    aula 2

---
+++ main.kt
@@ -0,0 +1,10 @@
+fun main (lojadevarejo) {
+    // variavel
+    var luis = Vendedor()
+    var importsAtacado = Fornecedor()
+    var roupa = Produto
+    var joao = Cliente ()
+    var lebreTransportes = Transportadora()
+    var venda = venda()
+}
+
